054be879a050e482bb7a854d22cde9925d9a7f05
Novr_wz05 <Novr_wz05@STHQ0122-04>
2022-09-12 08:55:55 +0400

    Add project files.

---
+++ Func/MyClass.cs
@@ -0,0 +1,5 @@
+﻿class MyClass
+{
+	public void Space(string s) => Console.WriteLine($"Space : {string.Join("_", s.ToCharArray())}");
+	public void Reverse(string s) => Console.WriteLine($"Reverse : {new string(s.Reverse().ToArray())}");
+}
---
+++ Func/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Func;
+
+class Run
+{
+	public void RunFunc(Func funcDell, string text) => funcDell(text);
+}
+
+delegate void Func(string text);
+class Program
+{
+	static void Main()
+	{
+		
+		Console.Write("Daxil et : ");
+		var s = Console.ReadLine();
+		MyClass cls = new MyClass();
+		Func funcDell = cls.Reverse;
+		funcDell += cls.Space;
+		Run run = new Run();
+		run.RunFunc(funcDell, s);
+
+	}
+}
